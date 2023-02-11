ec06ee4e43dcff8011261b36d0e250b7615bfc5a
Anton Trifonov <toha62@inbox.ru>
2023-02-11 17:32:50 +0300

    add test

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,9 @@
 export default class Validator {
-  validateUserName(name) {
-    return;
+  constructor(name) {
+    this.name = name;
+  }
+
+  validateUserName() {
+    return /^[a-zA-Z]\d{0,3}?([a-zA-Z_-]|[a-zA-Z_-]\d{0,3})*[a-zA-Z]$/.test(this.name);
   }
 }
