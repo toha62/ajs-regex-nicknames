ab2712ceadae462f6b8af897c0087f9374642a99
Anton Trifonov <toha62@inbox.ru>
2023-02-12 17:54:55 +0300

    fix app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,17 @@ export default class Validator {
   }
 
   validateUserName() {
-    return /^[a-zA-Z]\d{0,3}?([a-zA-Z_-]|[a-zA-Z_-]\d{0,3})*[a-zA-Z]$/.test(this.name);
+    const exeptStartWithDigit = !/^\d/.test(this.name);
+    const exeptEndWithDigit = !/\d$/.test(this.name);
+    const exeptStartWithDash = !/^[-]/.test(this.name);
+    const exeptEndWithDash = !/[-]$/.test(this.name);
+    const exeptStartWithUnderline = !/^[_]/.test(this.name);
+    const exeptEndWithUnderline = !/[_]$/.test(this.name);
+    const exeptThreeNumbersInRow = !/\d{4,}/g.test(this.name);
+    const acceptLatinDigitDashUnderline = !/[^\w-]/g.test(this.name);
+
+    return exeptStartWithDigit && exeptEndWithDigit && exeptStartWithDash && exeptEndWithDash
+      && exeptStartWithUnderline && exeptEndWithUnderline && exeptThreeNumbersInRow
+      && acceptLatinDigitDashUnderline;
   }
 }
